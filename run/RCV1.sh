937f6c0ae73780905c55364a60fefd8e3f6a0d8c
FlavioSoriano <flaviosoriano0@gmail.com>
2026-03-28 14:25:12 -0300

    RCVV! configs

---
+++ run/RCV1.sh
@@ -0,0 +1,123 @@
+# overrides
+data=RCV1
+model=RetrieverRoBERTa
+
+text_max_length=256
+label_max_length=256
+label_enhancement=RAW
+text_features_source=TXT
+
+# sparse_retrieve
+for fold_idx in $(seq $1 $2);
+do
+  time_start=$(date '+%Y-%m-%d %H:%M:%S')
+  python main.py \
+    tasks=[sparse_retrieve] \
+    model=BM25 \
+    data=$data \
+    data.text_features_source=$text_features_source \
+    data.folds=[$fold_idx]
+  time_end=$(date '+%Y-%m-%d %H:%M:%S')
+  echo "$time_start,$time_end" > resource/time/sparse_retrieve_${data}_${fold_idx}.tmr
+done
+
+# dense_retrieve fit
+for fold_idx in $(seq $1 $2);
+do
+  time_start=$(date '+%Y-%m-%d %H:%M:%S')
+  python main.py \
+    tasks=[fit] \
+    trainer.max_epochs=5 \
+    trainer.patience=3 \
+    model=$model \
+    model.name=${label_enhancement}_${model} \
+    data=$data \
+    data.text_max_length=$text_max_length \
+    data.label_max_length=$label_max_length \
+    data.label_enhancement=$label_enhancement \
+    data.text_features_source=$text_features_source \
+    data.batch_size=32 \
+    data.num_workers=12 \
+    data.folds=[$fold_idx]
+  time_end=$(date '+%Y-%m-%d %H:%M:%S')
+  echo "$time_start,$time_end" > resource/time/fit_${label_enhancement}_${model}_${data}_${fold_idx}.tmr
+done
+
+# dense_retrieve predict
+for fold_idx in $(seq $1 $2);
+do
+  time_start=$(date '+%Y-%m-%d %H:%M:%S')
+  python main.py \
+    tasks=[predict] \
+    trainer.max_epochs=5 \
+    trainer.patience=3 \
+    model=$model \
+    model.name=${label_enhancement}_${model} \
+    data=$data \
+    data.text_max_length=$text_max_length \
+    data.label_max_length=$label_max_length \
+    data.label_enhancement=$label_enhancement \
+    data.text_features_source=$text_features_source \
+    data.batch_size=64 \
+    data.num_workers=12 \
+    data.folds=[$fold_idx]
+  time_end=$(date '+%Y-%m-%d %H:%M:%S')
+  echo "$time_start,$time_end" > resource/time/predict_${label_enhancement}_${model}_${data}_${fold_idx}.tmr
+done
+
+# dense_retrieve eval
+for fold_idx in $(seq $1 $2);
+do
+  time_start=$(date '+%Y-%m-%d %H:%M:%S')
+  python main.py \
+    tasks=[eval] \
+    trainer.max_epochs=5 \
+    trainer.patience=3 \
+    model=$model \
+    model.name=${label_enhancement}_${model} \
+    data=$data \
+    data.text_max_length=$text_max_length \
+    data.label_max_length=$label_max_length \
+    data.label_enhancement=$label_enhancement \
+    data.text_features_source=$text_features_source \
+    data.batch_size=64 \
+    data.num_workers=12 \
+    data.folds=[$fold_idx]
+  time_end=$(date '+%Y-%m-%d %H:%M:%S')
+  echo "$time_start,$time_end" > resource/time/eval_${label_enhancement}_${model}_${data}_${fold_idx}.tmr
+done
+
+# fuse
+for fold_idx in $(seq $1 $2);
+do
+  time_start=$(date '+%Y-%m-%d %H:%M:%S')
+  python main.py \
+    tasks=[fuse] \
+    model=$model \
+    model.name=${label_enhancement}_V02_${model} \
+    data=$data \
+    data.text_features_source=$text_features_source \
+    data.folds=[$fold_idx]
+  time_end=$(date '+%Y-%m-%d %H:%M:%S')
+  echo "$time_start,$time_end" > resource/time/fuse_${label_enhancement}_${model}_${data}_${fold_idx}.tmr
+done
+
+# aggregate
+for fold_idx in $(seq $1 $2);
+do
+  time_start=$(date '+%Y-%m-%d %H:%M:%S')
+  python main.py \
+    tasks=[aggregate] \
+    model=$model \
+    model.name=${label_enhancement}_V02_${model} \
+    data=$data \
+    data.text_max_length=$text_max_length \
+    data.label_max_length=$label_max_length \
+    data.label_enhancement=$label_enhancement \
+    data.text_features_source=$text_features_source \
+    data.batch_size=64 \
+    data.num_workers=12 \
+    data.folds=[$fold_idx]
+  time_end=$(date '+%Y-%m-%d %H:%M:%S')
+  echo "$time_start,$time_end" > resource/time/aggregate_${label_enhancement}_${model}_${data}_${fold_idx}.tmr
+done
